ff00cff3a94ab21d0332319c71808833f8692d47
Matt's Linux Desktop <mnicholas@g.ucla.edu>
2020-04-21 23:53:39 -0700

    compiles and works for host code. Make file updated. GPU is single block, thread but not working

--- Makefile
+++ Makefile
@@ -1,19 +1,35 @@
-.PHONY: all
-all: clean main
+# .PHONY: all
+# all: clean classifiers
 
-main: 
-	nvcc -I ../NVIDIA_CUDA-10.1_Samples/common/inc/ -o main main.cu
+# classifiers: 
+# 	nvcc -I ../NVIDIA_CUDA-10.1_Samples/common/inc/ -o classifiers classifiers.cu
 
-.Phony: run
-run: clean main
-	./main
+# .Phony: run
+# run: clean classifiers
+# 	./classifiers
 
-.Phony: prof
-prof: run
-	nvprof ./main
+# .Phony: prof
+# prof: run
+# 	nvprof ./classifiers
 
-.Phony: clean
+# .Phony: clean
+# clean:
+# 	rm -f classifiers
+
+
+
+CFLAGS=$(OPT) --std=c++11 -g -O3
+MODULE  := class1
+
+.PHONY: all clean
+
+all: $(MODULE)
+
+HEADERS=dnn.hpp
+INCLUDES  = -I ../NVIDIA_CUDA-10.1_Samples/common/inc
+
+class1: classifiers.cu 
+	nvcc $^ $(CFLAGS) $(INCLUDES) -o $@ -DNi=25088 -DNn=4096 -DTii=512 -DTi=64 -DTnn=32  -DTn=16
+
 clean:
-	rm -f main
-
-
+	@rm -f $(MODULE) 
--- classifiers.cu
+++ classifiers.cu
@@ -1,7 +1,13 @@
 #include <iostream>
 #include "dnn.hpp"
 
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <helper_functions.h>
+#include <helper_cuda.h>
+
 using namespace std;
+
 
 // this will be called in main() once we have tiling values figured out
 #ifndef Tii
@@ -14,34 +20,44 @@ using namespace std;
  #define Ti 16
 #endif
 
+  //Arrays:
+VTYPE weights[Nn][Ni] __attribute__((aligned(64)));
+VTYPE data_in[Ni] __attribute__((aligned(64)));
+VTYPE data_out[Nn] __attribute__((aligned(64)));
+VTYPE data_out_block[Nn] __attribute__((aligned(64)));
+VTYPE data_out_gpu[Nn] __attribute__((aligned(64)));
+
+
+
 void fill_classifier(VTYPE (&weights)[Nn][Ni], VTYPE (&data_in)[Ni],
-   VTYPE (&data_out)[Nn], VTYPE (&data_out_tiled)[Nn]) {
- for(int n = 0; n < Nn; ++n) {
-   for(int i = 0; i < Ni; ++i) {
-     weights[n][i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX) - 0.5f;
-   }
- }
- for(int i = 0; i < Ni; ++i) {
-   data_in[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX) - 0.5f;
- }
- for(int n = 0; n < Nn; ++n) {
-   data_out[n] = 0; //i;
-   data_out_tiled[n] = 0;
- }
+   VTYPE (&data_out)[Nn], VTYPE (&data_out_block)[Nn]) {
+
+  for(int n = 0; n < Nn; ++n) {
+    for(int i = 0; i < Ni; ++i) {
+      weights[n][i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX) - 0.5f;
+      weights[n][i] = n;
+    }
+  }
+  for(int i = 0; i < Ni; ++i) {
+    data_in[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX) - 0.5f;
+  }
+  for(int n = 0; n < Nn; ++n) {
+    data_out[n] = 0; 
+    data_out_block[n] = 0;
+  }
 }
 
-__global__ void classifier_layer(VTYPE (&d_weights)[Nn][Ni], VTYPE (&d_data_in)[Ni], VTYPE (&d_data_out)[Nn]) {
+__global__ void classifier_layer_gpu(VTYPE *d_weights, VTYPE *d_data_in, VTYPE *d_data_out) {
   // blockDim = threads in block
   int ix = blockIdx.x*blockDim.x+threadIdx.x;
   int iy = blockIdx.y*blockDim.y+threadIdx.y;
-
   if(ix * iy < Ni * Nn){
-    VTYPE tmp = 0
-    for (int m = 0; m < Nn; m++) {
-      for (int n = 0; n < Ni; n++) {
-        tmp += d_data_in[n] * d_weights[n][m]
+    for (int n= 0; n < Nn; n++) {
+      VTYPE tmp = 0;
+      for (int i = 0; i < Ni; i++) {
+        tmp += d_weights[i*n + i] * d_data_in[i];
       }
-      data_out[n] = transfer(tmp)
+      d_data_out[n] = tmp;
     }
   }
 }
@@ -58,9 +74,8 @@ void classifier_layer_host(VTYPE (&weights)[Nn][Ni], VTYPE (&data_in)[Ni], VTYPE
 }
 
 // not yet converted to CUDA operation
-void classifier_layer_blocked(VTYPE (&synapse)[Nn][Ni], VTYPE (&neuron_i)[Ni],
-                             VTYPE (&neuron_n)[Nn]) {
- int total_calc=0;
+void classifier_layer_blocked_host(VTYPE (&weights)[Nn][Ni], VTYPE (&data_in)[Ni],
+                             VTYPE (&data_out_block)[Nn]) {
  VTYPE sum[Nn]={0};
  for (int nnn = 0; nnn < Nn; nnn += Tnn) { // tiling for output neurons;
    for (int iii = 0; iii < Ni; iii += Tii) { // tiling for input neurons;
@@ -70,7 +85,7 @@ void classifier_layer_blocked(VTYPE (&synapse)[Nn][Ni], VTYPE (&neuron_i)[Ni],
          for (int n = nn; n < nn + Tn; n++) {
            VTYPE sum_sc=0;
            for (int i = ii; i < ii + Ti; i++) {
-             sum_sc += (synapse[n][i] * neuron_i[i]);
+             sum_sc += (weights[n][i] * data_in[i]);
            }
            sum[n]+=sum_sc;
          }
@@ -78,89 +93,63 @@ void classifier_layer_blocked(VTYPE (&synapse)[Nn][Ni], VTYPE (&neuron_i)[Ni],
      }
    }
    for (int nn = nnn; nn < nnn + Tnn; nn++) {
-     neuron_n[nn] = transfer(sum[nn]);
+     data_out_block[nn] = transfer(sum[nn]);
    }
  }
 }
 
 int main(int argc, char** argv) {
 
-  if (argc == 0) {
-    printf("Too few arguments, requires one argument passed.");
-  } else if (argc > 1) {
-    printf("Too many arguments, requires one argument passed.");
-  } else if (argv[0] == 1) {
-    #define Nn=4096
-    #define Ni=25088
-    // add tiling values here per case
-  } else if (argv[1] == 2) {
-    #define Nn=1024
-    #define Ni=4096
-    // add tiling values here per case
-  } else {
-    printf("Unrecognized input argument");
-  }
-
-  //Arrays:
-  VTYPE weights[Nn][Ni] __attribute__((aligned(64)));
-  VTYPE data_in[Ni] __attribute__((aligned(64)));
-  VTYPE data_out[Nn] __attribute__((aligned(64)));
-  VTYPE data_out_tmp[Nn] __attribute__((aligned(64)));
-  VTYPE data_out_tiled[Nn] __attribute__((aligned(64)));
-
-  // allocate arrays in device memory
-  float *d_data_in, *d_weights, *d_data_out, *d_data_out_tiled
-  cudaMalloc(&d_data_in, inputSize)
-  cudaMalloc(&d_weights, weightsSize)
-  cudaMalloc(&d_data_out, outputSize)
-  cudaMalloc(&d_data_out_tiled, outputSize)
 
   cout << "initializing arrays\n";
 
-  // fill arrays in host memory
-  //fill_classifier(synapse,neuron_i,neuron_n,neuron_n2);
-  fill_classifier(weights,data_in,data_out,d_data_out_tmp)
-
-  // transfer data to device
-  cudaMemcpy(d_data_in, data_in, cudaMemcpyHostToDevice)
-  cudaMemcpy(d_weights, weights, cudaMemcpyHostToDevice)
-  cudaMemcpy(d_data_out, data_out, cudaMemcpyHostToDevice)
-  cudaMemcpy(d_data_out_tiled, data_out_tiled, cudaMemcpyHostToDevice)
-
-  // need to define number of threads per block and number of blocks
-  dim3 blocksPerGrid(1,1,1)
-  // threads per block cannot exceed 1024 total x1*x2*x3
-  dim3 threadsPerBlock(1,1,1)
-
+  fill_classifier(weights,data_in,data_out,data_out_block);
   cout << "starting computation\n";
 
   begin_roi();
-  classifier_layer<<<blocksPerGrid,threadsPerBlock>>>(d_weights,d_data_in,d_data_out);
+  classifier_layer_host(weights,data_in,data_out);
   end_roi();
-
-  // synchronize thread execuation and copy results back to host
-  cudaDeviceSynchronize();
-  cudaMemcpy(data_out, d_data_out, cudaMemcpyDeviceToHost)
-
-  // test to make sure our computations are correct
-  begin_roi();
-  classifier_layer_host(weights,data_in,data_out_tmp);
-  end_roi();
-  compare(data_out,data_out_tmp,Nn);
 
   cout << "simple version complete!\n";
 
-  //begin_roi();
-  //classifier_layer_blocked<<<numBlocks,blockSize>>>(d_weights,d_data_in,d_data_out_tiled);
-  //end_roi();
+  begin_roi();
+  classifier_layer_blocked_host(weights,data_in,data_out_block);
+  end_roi();
 
-  // synchronize thread execuation and copy results back to host
-  //cudaDeviceSynchronize();
-  //cudaMemcpy(data_out_tiled, d_data_out_tiled, cudaMemcpyDeviceToHost)
+  cout << "blocked computation complete!\n";
 
-  //cout << "blocked computation complete!\n";
+  // allocate arrays in device memory
+  int inputSize = sizeof(VTYPE)*Ni;
+  int weightsSize = sizeof(VTYPE)*Ni*Nn;
+  int outputSize = sizeof(VTYPE)*Nn;
+  VTYPE *d_data_in, *d_weights, *d_data_out;
+  cudaMalloc(&d_data_in, inputSize);
+  cudaMalloc(&d_weights, weightsSize);
+  cudaMalloc(&d_data_out, outputSize);
 
-  compare(data_out,data_out_tiled,Nn);
+  // transfer data to device
+  cudaMemcpy(d_data_in, &data_in, inputSize, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_weights, &weights, weightsSize, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_data_out, &data_out, outputSize, cudaMemcpyHostToDevice);
+  
+
+  int blockSize = 1;
+  int numBlocks = 1;
+
+
+  begin_roi();
+  classifier_layer_gpu<<<numBlocks,blockSize>>>(d_weights,d_data_in,d_data_out);
+  cudaDeviceSynchronize();
+  end_roi();
+  cout << "Cuda Done\n";
+
+  cudaMemcpy(&data_out_gpu, d_data_out, outputSize, cudaMemcpyDeviceToHost);
+
+  //compare(data_out, data_out_gpu, Nn);
+
+  cudaFree(d_data_in);
+  cudaFree(d_data_out);
+  cudaFree(d_weights);
 
   cout << "done\n";
 }
